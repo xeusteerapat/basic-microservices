ac7ede6899c6b61ac9663d11adc7dea68dd57c8d
xeusteerapat <xeus085@gmail.com>
2021-01-12 22:16:05 +0700

    [EDIT] mock post

--- posts/index.js
+++ posts/index.js
@@ -1,14 +1,30 @@
 const express = require('express');
 const cors = require('cors');
+const { randomBytes } = require('crypto');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get('/', (req, res) => {});
+const posts = {};
 
-app.post('/', (req, res) => {});
+app.get('/posts', (req, res) => {
+  res.send(posts);
+});
+
+app.post('/posts', (req, res) => {
+  const id = randomBytes(4).toString('hex');
+
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
 
 app.listen(4000, () => {
   console.log('Posts services is listening on port 4000');
