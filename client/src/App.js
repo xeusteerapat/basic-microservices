ffbcdb09b06882c3010a94b3b4dc97aa8bb159d2
xeusteerapat <xeus085@gmail.com>
2021-01-13 08:42:02 +0700

    [EDIT] render post list

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import PostCreate from './PostCreate';
+import PostList from './PostList';
 
 function App() {
   return (
     <div className='container'>
       <h1>Blog App</h1>
       <PostCreate />
+      <hr />
+      <h1>List of Posts</h1>
+      <PostList />
     </div>
   );
 }
