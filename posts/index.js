f5017e50cc900bd37cec1756c6e800e99591bc01
xeusteerapat <xeus085@gmail.com>
2021-01-13 22:50:37 +0700

    [EDIT] post and comment to event bus

--- posts/index.js
+++ posts/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const { randomBytes } = require('crypto');
+const axios = require('axios');
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.get('/posts', (req, res) => {
   res.send(posts);
 });
 
-app.post('/posts', (req, res) => {
+app.post('/posts', async (req, res) => {
   const id = randomBytes(4).toString('hex');
 
   const { title } = req.body;
@@ -22,6 +23,15 @@ app.post('/posts', (req, res) => {
     id,
     title,
   };
+
+  // post event to event bus
+  await axios.post('http://localhost:4005/events', {
+    type: 'PostCreated',
+    data: {
+      id,
+      title,
+    },
+  });
 
   res.status(201).send(posts[id]);
 });
