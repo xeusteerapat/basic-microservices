17243d0b646314159ed75d09fe1ec5711b051a2a
xeusteerapat <xeus085@gmail.com>
2021-01-13 21:41:15 +0700

    [EDIT] post request comment

--- client/src/CommentCreate.js
+++ client/src/CommentCreate.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 
 const CommentCreate = ({ postId }) => {
+  const [content, setContent] = useState('');
+
+  const onSubmit = async e => {
+    e.preventDefault();
+
+    await axios.post(`http://localhost:4001/posts/${postId}/comments`, {
+      content,
+    });
+
+    setContent('');
+  };
+
   return (
     <div>
-      <form action=''>
+      <form onSubmit={onSubmit}>
         <div className='form-group'>
           <label htmlFor=''>New Comment</label>
-          <input type='text' className='form-control' />
+          <input
+            type='text'
+            className='form-control'
+            value={content}
+            onChange={e => setContent(e.target.value)}
+          />
         </div>
         <button className='btn btn-primary'>Add Comment</button>
       </form>
