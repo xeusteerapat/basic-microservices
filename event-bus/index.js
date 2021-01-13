8b0f2accc02a4f34543ea1836c0e472359c21dfa
xeusteerapat <xeus085@gmail.com>
2021-01-13 22:37:42 +0700

    [INIT] event bus

---
+++ event-bus/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+
+app.post('/events', (req, res) => {
+  const events = req.body;
+
+  axios.post('http://localhost:4000/events', events);
+  axios.post('http://localhost:4001/events', events);
+  axios.post('http://localhost:4002/events', events);
+
+  res.send({ status: 'OK' });
+});
+
+app.listen(4005, () => {
+  console.log('Event bus listening on port 4005');
+});
