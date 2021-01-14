7a404f4abf9e18f54f9651c8ce4066848488217e
xeusteerapat <xeus085@gmail.com>
2021-01-14 22:01:30 +0700

    [ADD] moderation service

---
+++ moderation/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/events', (req, res) => {});
+
+app.listen(4003, () => {
+  console.log('Moderation services is listening on port 4003');
+});
