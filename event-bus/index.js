1c9e5614399c14e141d9c47a4662d259f6b073c7
xeusteerapat <xeus085@gmail.com>
2021-01-14 22:11:51 +0700

    [EDIT] handling event in moderation service

--- event-bus/index.js
+++ event-bus/index.js
@@ -10,6 +10,7 @@ app.post('/events', (req, res) => {
   axios.post('http://localhost:4000/events', events);
   axios.post('http://localhost:4001/events', events);
   axios.post('http://localhost:4002/events', events);
+  axios.post('http://localhost:4003/events', events);
 
   res.send({ status: 'OK' });
 });
--- moderation/index.js
+++ moderation/index.js
@@ -5,7 +5,25 @@ const app = express();
 
 app.use(express.json());
 
-app.post('/events', (req, res) => {});
+app.post('/events', async (req, res) => {
+  const { type, data } = req.body;
+
+  if (type === 'CommentCreated') {
+    const status = data.content.includes('orange') ? 'rejected' : 'approved';
+
+    await axios.post('http://localhost:4005/events', {
+      type: 'CommentCreated',
+      data: {
+        id: data.id,
+        postId: data.postId,
+        status,
+        content: data.content,
+      },
+    });
+  }
+
+  res.send({});
+});
 
 app.listen(4003, () => {
   console.log('Moderation services is listening on port 4003');
