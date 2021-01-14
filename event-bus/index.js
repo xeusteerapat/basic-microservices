4a6f4836dd1d06e0eb531863f9c894c758f0ec06
xeusteerapat <xeus085@gmail.com>
2021-01-14 23:07:37 +0700

    [EDIT] store event in events bus

--- event-bus/index.js
+++ event-bus/index.js
@@ -4,15 +4,23 @@ const axios = require('axios');
 const app = express();
 app.use(express.json());
 
-app.post('/events', (req, res) => {
-  const events = req.body;
+const events = [];
 
-  axios.post('http://localhost:4000/events', events);
-  axios.post('http://localhost:4001/events', events);
-  axios.post('http://localhost:4002/events', events);
-  axios.post('http://localhost:4003/events', events);
+app.post('/events', (req, res) => {
+  const event = req.body;
+
+  events.push(event);
+
+  axios.post('http://localhost:4000/events', event);
+  axios.post('http://localhost:4001/events', event);
+  axios.post('http://localhost:4002/events', event);
+  axios.post('http://localhost:4003/events', event);
 
   res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
