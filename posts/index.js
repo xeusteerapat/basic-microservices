5d6896972f93d1fa5a00ab17a149e62fcbfd03fb
xeusteerapat <xeus085@gmail.com>
2021-01-17 16:44:18 +0700

    [EDIT] update service addresses for posts and event-bus

--- posts/index.js
+++ posts/index.js
@@ -25,7 +25,7 @@ app.post('/posts', async (req, res) => {
   };
 
   // post event to event bus
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id,
